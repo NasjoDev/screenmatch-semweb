475d88475ed4d7b0f7d3e9ce135c093676aefaad
NasjoDev <jonas_nasjo.jp@hotmail.com>
2024-10-29 14:50:33 -0300

    Alterando filtros por data

--- Screenmatch/src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ Screenmatch/src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -8,6 +8,8 @@ import br.com.alura.Screenmatch.service.ConsumoAPI;
 import br.com.alura.Screenmatch.service.ConverteDados;
 
 import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -61,7 +63,23 @@ public class Principal {
                 .flatMap(t -> t.episodios().stream()
                         .map(d-> new Episodio(t.numero(), d))
                 ).collect(Collectors.toList());
+
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios ?");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada:  " + e.getTemporada() +
+                                "Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
 
     }
 }
